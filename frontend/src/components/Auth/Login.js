06c207cdc1cca39297f6663273ffb69f8f7efbb2
GAJENDER <mandiwalgajender0001@gmail.com>
2025-08-23 03:30:29 +0530

    Login route debugging

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -33,6 +33,7 @@ const Login = () => {
 
     try {
       const API_URL = process.env.REACT_APP_API_URL;
+      console.log("API_URL:", API_URL);
 
       const response = await fetch(`${API_URL}/login`, {
         method: 'POST',
