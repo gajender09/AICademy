3d5c989d7edf7b844f792fda65a3290df3efe8b4
GAJENDER <mandiwalgajender0001@gmail.com>
2025-08-23 19:12:28 +0530

    v3

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -32,7 +32,7 @@ const Login = () => {
     }
 
     try {
-      const API_URL = "https://aicademy-cjr2.onrender.com";
+      const API_URL = process.env.REACT_APP_API_URL;
       console.log("API_URL:", API_URL);
 
       const response = await fetch(`${API_URL}/login`, {
